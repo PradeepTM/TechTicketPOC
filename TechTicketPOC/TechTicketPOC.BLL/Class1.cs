8ba9fedb37445459abb38c0872c3ce1a4f8c9ba7
Pradeep TM <pradee47cs@gmail.com>
2019-08-16 13:52:18 +0530

    Tech Ticket POC

---
+++ TechTicketPOC/TechTicketPOC.BLL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTicketPOC.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ TechTicketPOC/TechTicketPOC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TechTicketPOC.Global" Language="C#" %>
